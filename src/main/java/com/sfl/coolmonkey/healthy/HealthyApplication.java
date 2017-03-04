dbf4f94d9f70217a828cc4ba0838380e813f9423
Arthur Asatryan <biacoder@gmail.com>
2017-03-04 14:42:37 +0400

    removed unnecessary endpoints

--- src/main/java/com/sfl/coolmonkey/healthy/HealthyApplication.java
+++ src/main/java/com/sfl/coolmonkey/healthy/HealthyApplication.java
@@ -48,25 +48,7 @@ public class HealthyApplication {
             if (strings.length > 0 && "init".equals(strings[0])) {
                 // TODO: Add "environment" logic, so we can get all endpoints by environment setting
                 // TODO: Add email credentials to "environment", so we will not hardcode the emails
-                healthCheckEndpointService.create("website", "http://website");
-                healthCheckEndpointService.create("gateway", "http://gatewayms:8080/api/heartbeat");
-                healthCheckEndpointService.create("company", "http://companyms:8081/heartbeat");
-                healthCheckEndpointService.create("companyms-scheduler", "http://companyms-scheduler:9094");
-                healthCheckEndpointService.create("address", "http://addressms:8082/heartbeat");
-                healthCheckEndpointService.create("addressms-consumer", "http://addressms-consumer:9091");
-                healthCheckEndpointService.create("addressms-scheduler", "http://addressms-scheduler:9097");
-                healthCheckEndpointService.create("emailtemplate", "http://emailtemplatems:8088/heartbeat");
-                healthCheckEndpointService.create("notifications", "http://notificationsms:8083/heartbeat");
-                healthCheckEndpointService.create("notificationsms-consumer", "http://notificationsms-consumer:9092");
-                healthCheckEndpointService.create("translations", "http://translationsms:8084/heartbeat");
-                healthCheckEndpointService.create("coolfs", "http://coolfsms:8089/heartbeat");
-                healthCheckEndpointService.create("paymentms", "http://paymentms:8085/rest/heartbeat");
-                healthCheckEndpointService.create("paymentms-scheduler", "http://paymentms-scheduler:9093");
-                healthCheckEndpointService.create("coolsearch", "http://coolsearchms:8086/heartbeat");
-                healthCheckEndpointService.create("reporting", "http://reportingms:8091/heartbeat");
-                healthCheckEndpointService.create("reportingms-consumer", "http://reportingms-consumer:9096");
-                healthCheckEndpointService.create("reportingms-scheduler", "http://reportingms-scheduler:9095");
-                healthCheckEndpointService.create("wkhtmltopdf-docker-http", "http://wkhtmltopdf-docker-http:8087");
+                healthCheckEndpointService.create("my-microservice-name", "http://my-microservice-url.com");
             }
         }
     }
